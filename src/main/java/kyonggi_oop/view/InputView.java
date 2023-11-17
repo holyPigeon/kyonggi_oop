0e060ebd296ec1b1993d871928da788284163652
jae woo Jung <awfjol2008@naver.com>
2023-11-17 10:39:59 +0900

    fix: 초기 데이터 입력이 실패하는 오류 수정 (#6)
    * docs: 기능 구현 목록 작성
    
    * docs: 기능 구현 목록 수정(기능 추가)
    
    * feat: 좌석 이용 관련 기능 구현
    
    * docs: 기능 구현 완료 목록 반영
    
    * fix: 좌석 반납 후 사용한 좌석이 여전히 이용 불가인 문제 수정
    
    * feat: 예외 반환 시 입력값을 다시 받도록 수정
    
    * feat: 로그인 기능 구현
    
    * fix: 초기 데이터 입력이 실패하는 오류 수정

--- src/main/java/kyonggi_oop/view/InputView.java
+++ src/main/java/kyonggi_oop/view/InputView.java
@@ -4,7 +4,8 @@ import kyonggi_oop.domain.seat.RoomType;
 import kyonggi_oop.domain.seat.Seat;
 import kyonggi_oop.domain.user.User;
 
-import java.util.Arrays;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,33 +18,40 @@ public class InputView {
     }
 
     public static List<Seat> readSeats() {
-        System.out.println("좌석 데이터를 입력합니다.");
-        String input = scanner.next();
-        return Arrays.stream(input.split("\n"))
-                .map(line -> {
-                    String[] parts = line.split(", ");
-                    int roomNumber = Integer.parseInt(parts[1]);
-                    RoomType roomType = RoomType.findByName(parts[0]);
-                    return new Seat(roomNumber, roomType);
-                }).toList();
+        System.out.println("좌석 데이터를 입력하고, 입력 종료 시에는 \"입력 종료\"를 입력합니다.");
+
+        List<Seat> seats = new ArrayList<>();
+        String line;
+        while (!(line = scanner.nextLine().trim()).equals("입력 종료")) {
+            String[] parts = line.split(",");
+            int roomNumber = Integer.parseInt(parts[1]);
+            RoomType roomType = RoomType.findByName(parts[0]);
+            seats.add(new Seat(roomNumber, roomType));
+        }
+
+        return seats;
     }
 
     public static List<User> readUsers() {
-        System.out.println("사용자 데이터를 입력합니다.");
-        String input = scanner.nextLine();
-        return Arrays.stream(input.split("\n"))
-                .map(line -> {
-                    String[] parts = line.split(", ");
-                    String studentId = parts[0];
-                    String password = parts[1];
-                    return new User(studentId, password);
-                }).toList();
+        System.out.println("사용자 데이터를 입력하고, 입력 종료 시에는 \"입력 종료\"를 입력합니다.");
+
+        List<User> users = new ArrayList<>();
+        String line;
+        while (!(line = scanner.nextLine().trim()).equals("입력 종료")) {
+            String[] parts = line.split(",");
+            String studentId = parts[0].trim();
+            String password = parts[1].trim();
+            users.add(new User(studentId, password));
+        }
+
+        return users;
     }
 
     public static User readStudentIdAndPassword() {
-        System.out.println("학번과 비밀번호를 입력해주세요. (e.g. 202301234, abc123");
+        System.out.println("학번과 비밀번호를 입력해주세요. (e.g. 202301234,abc123)");
         String input = scanner.next();
-        String[] split = input.split(input);
+        String[] split = input.split(",");
+
         return new User(split[0], split[1]);
     }
 
